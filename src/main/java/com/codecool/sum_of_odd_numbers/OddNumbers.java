dddbacc7d9e5f083d4324b06c3acc30e8a05a142
tothmesz <66495712+tothmesz@users.noreply.github.com>
2021-03-24 17:17:56 +0100

    finished

--- src/main/java/com/codecool/sum_of_odd_numbers/OddNumbers.java
+++ src/main/java/com/codecool/sum_of_odd_numbers/OddNumbers.java
@@ -3,8 +3,13 @@ package com.codecool.sum_of_odd_numbers;
 import java.util.List;
 
 public class OddNumbers {
-
     public static Integer sum(List<Integer> numbers) {
-        return null;
+        int sum = 0;
+        for (int number : numbers) {
+            if (number % 2 != 0) {
+                sum += number;
+            }
+        }
+        return sum;
     }
 }
